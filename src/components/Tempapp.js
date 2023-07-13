e57f6a6b1583b566d3342f3f070f973ffcf2718a
nsdhl <dhlnischal10@gmail.com>
2023-07-13 21:30:06 +0545

    feat: updated hooks

--- src/components/Tempapp.js
+++ src/components/Tempapp.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./css/style.css";
 
 const Tempapp = () => {
+
+
+   const [city, setCity] = useState(null);
+    const [search, setSearch] = useState("Mumbai");
+
+    useEffect( () => {
+        const fetchApi = async () => {
+          const url = `http://api.openweathermap.org/data/2.5/weather?q=pokhara&appid=9353169fadb314c9e78c3c8df648fb96`
+          const response = await fetch(url);
+          const resJson = await response.json();    
+        }
+    fetchApi();
+    }, [search])
   return (
     <>
       <div className="box">
@@ -11,19 +24,19 @@ const Tempapp = () => {
 
         <div className="info">
           <h2 className="location">
-            <i class="fa-solid fa-street-view">Kathmandu </i>
+            <i class="fa-solid fa-street-view">{city}</i>
           </h2>
 
           <h1 className="temp">5°C</h1>
           <h3 className="tempmin_max">Min: 5°C | Max: 5°C</h3>
         </div>
 
-        <div className="wave -one"></div>
-        <div className="wave -two"></div>
-        <div className="wave -three"></div>
+        <div className="waveone"></div>
+        <div className="wavetwo"></div>
+        <div className="wavethree"></div>
       </div>
     </> // React Fragment
-  );
+  )
 };
 
 export default Tempapp;
